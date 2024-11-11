1211c69febe8a546b628b858728108e38466ca36
MizhgonaRustami <mizhgonarustami@gmail.com>
2024-11-11 17:17:06 +0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="login_process.php" method="post">
+    <label for="">Логин</label><br>
+ <input type="text" name="login" placeholder="Логин" required>
+ <label for="">Пароль</label><br>
+ <input type="password" name="password" placeholder="Пароль"
+required><br>
+ <button type="submit">Войти</button><br>
+</form>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="register_process.php" method="post">
+    <label for="">Логин</label><br>
+ <input type="text" name="login" placeholder="Логин" required><br>
+ <label for="">Пароль</label><br>
+ <input type="password" name="password" placeholder="Пароль"
+required><br>
+ <button type="submit">Зарегистрироваться</button><br>
+</form>
+
+</body>
+</html>
